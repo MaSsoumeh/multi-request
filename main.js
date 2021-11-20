1bf8155ae6c7c19b27b9fee361012050bb924b8c
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-20 04:08:28 +0330

    add hr as seperator

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 // TODO
 document.getElementById('send-requests').addEventListener('click', sendRequest);
-
+const resultsDiv = document.getElementById('results');
 function sendRequest() {
-  document.getElementById('results').innerHTML = '';
+  resultsDiv.innerHTML = '';
 
   let requests = document.getElementById('urls').value;
   requests = requests.trim();
   const splitReq = requests.split(/\n+|\s+/);
   splitReq.map((request) => {
     const resDiv = document.createElement('div');
+    const hr = document.createElement('hr');
     myFetch(request)
       .then((jsonResponse) => {
         resDiv.innerHTML = JSON.stringify(jsonResponse);
       })
       .catch(() => (resDiv.innerHTML = 'error'));
-    document.getElementById('results').appendChild(resDiv);
+
+    resultsDiv.appendChild(resDiv);
+    resultsDiv.appendChild(hr);
   });
 
   async function myFetch(request) {
